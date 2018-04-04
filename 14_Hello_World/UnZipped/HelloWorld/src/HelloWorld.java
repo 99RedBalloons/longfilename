711acddca8046d25c9fb0c3ecd8903c2887f6756
Richard Arnold <richard.arnold@checkmarx.com>
2018-04-04 16:59:19 +0100

    'HelloWorld'

---
+++ 14_Hello_World/UnZipped/HelloWorld/src/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.example.HelloWorld;
+public class HelloWorld {
+    public static void main (String[] args){
+		int a = 1;
+		int b = 2;
+		int c = a + b;
+		if (a == 33) b = 6;
+        System.out.println("Hello World!");
+    }
+}
